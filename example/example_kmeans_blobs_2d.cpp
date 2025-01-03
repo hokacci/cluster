a2fb576793801a0d1ed7e5498d504c4f8ec78b57
hokacci <47231909+hokacci@>
2025-01-03 21:19:59 +0900

    Visualize with matplot++

--- example/example_kmeans_blobs_2d.cpp
+++ example/example_kmeans_blobs_2d.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <matplot/matplot.h>
+
 #include "blobs_2d.h"
 #include "kmeans.h"
 #include "point_2d.h"
@@ -12,10 +14,33 @@ int main()
 	std::vector<Point2D> centroids;
 	yhok::cluster::exec_kmeans(points, 3, 100, labels, centroids);
 
+	std::vector<double> x(points.size());
+	std::vector<double> y(points.size());
+	std::vector<double> c(points.size());
 	for (int i = 0; i < points.size(); ++i)
 	{
-		std::cout << labels[i] << "," << std::endl;
+		x[i] = points[i].x;
+		y[i] = points[i].y;
+		c[i] = static_cast<double>(labels[i]);
 	}
+
+	auto l = matplot::scatter(x, y, std::vector<double>{}, c);
+	l->marker_face(true);
+	l->marker_size(8);
+	matplot::hold(matplot::on);
+
+	std::vector<double> cx(centroids.size());
+	std::vector<double> cy(centroids.size());
+	for (int i = 0; i < centroids.size(); ++i)
+	{
+		cx[i] = centroids[i].x;
+		cy[i] = centroids[i].y;
+	}
+
+	auto cl = matplot::scatter(cx, cy, 30);
+	cl->marker_style(matplot::line_spec::marker_style::cross);
+
+	matplot::show();
 
 	return 0;
 }
