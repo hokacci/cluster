160da25b02a0d7f7ecd31f4e226af02814d2c954
hokacci <47231909+hokacci@>
2025-01-03 23:20:40 +0900

    Reduce copy

--- example/helpers/include/point_2d.h
+++ example/helpers/include/point_2d.h
@@ -1,18 +1,17 @@
 #pragma once
 
-#include <vector>
 #include <cmath>
-
+#include <vector>
 
 // 2次元の点を表す構造体
 struct Point2D
 {
-    double x = 0;
-    double y = 0;
+	double x = 0;
+	double y = 0;
 };
 
 // 2点間の距離を計算する
-double distance(Point2D a, Point2D b);
+double distance(const Point2D &a, const Point2D &b);
 
 // 重心を計算する
-Point2D center(std::vector<Point2D> points);
+Point2D center(const std::vector<Point2D> &points);
--- example/helpers/src/point_2d.cpp
+++ example/helpers/src/point_2d.cpp
@@ -1,11 +1,11 @@
 #include "point_2d.h"
 
-double distance(Point2D a, Point2D b)
+double distance(const Point2D &a, const Point2D &b)
 {
 	return std::sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 }
 
-Point2D center(std::vector<Point2D> points)
+Point2D center(const std::vector<Point2D> &points)
 {
 	Point2D c;
 	for (auto p : points)
