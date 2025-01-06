adbe2c4b3496b7351bcf9d8901b9a0a6844369fb
hokacci <47231909+hokacci@>
2025-01-06 11:20:31 +0900

    Count iterations

--- example/example_kmeans_plus_plus_blobs_2d.cpp
+++ example/example_kmeans_plus_plus_blobs_2d.cpp
@@ -7,32 +7,39 @@
 int main()
 {
 	yhok::cluster::KMeans kmeans(3, 100, dataset_blobs_2d);
+	int n = 10000000;
 
-	// k-meansを10000回実行して、失敗する回数を数える
+	// k-meansをn回実行して、失敗する回数を数える
 	int count_failure = 0;
-	for (int i = 0; i < 10000; ++i)
+	int sum_iterations = 0;
+	for (int i = 0; i < n; ++i)
 	{
-		kmeans.exec();
+		int iterations = kmeans.exec();
+		sum_iterations += iterations;
 
 		if (kmeans.labels[243] == kmeans.labels[21])
 		{
 			++count_failure;
 		}
 	}
-	std::cout << "Failure rate (k-means): " << count_failure / 10000.0 << std::endl;
+	std::cout << "Failure rate (k-means): " << count_failure / static_cast<double>(n) << std::endl;
+	std::cout << "Average number of iterations: " << sum_iterations / static_cast<double>(n) << std::endl;
 
-	// k-means++を10000回実行して、失敗する回数を数える
+	// k-means++をn回実行して、失敗する回数を数える
 	count_failure = 0;
-	for (int i = 0; i < 10000; ++i)
+	sum_iterations = 0;
+	for (int i = 0; i < n; ++i)
 	{
-		kmeans.exec_kmeans_plus_plus();
+		int iterations = kmeans.exec_kmeans_plus_plus();
+		sum_iterations += iterations;
 
 		if (kmeans.labels[243] == kmeans.labels[21])
 		{
 			++count_failure;
 		}
 	}
-	std::cout << "Failure rate (k-means++): " << count_failure / 10000.0 << std::endl;
+	std::cout << "Failure rate (k-means++): " << count_failure / static_cast<double>(n) << std::endl;
+	std::cout << "Average number of iterations: " << sum_iterations / static_cast<double>(n) << std::endl;
 
 	return 0;
 }
